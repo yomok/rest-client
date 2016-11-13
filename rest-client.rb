9747aa40bbaf31e6e3c62614ab8b18608c85538c
len lee <lenlee454@gmail.com>
2016-11-13 12:02:32 -0800

    updated google rest-client

--- rest-client.rb
+++ rest-client.rb
@@ -1,8 +1,14 @@
+
 require 'rest-client'
 puts "What would you like to search for?"
-answer = gets.chomp
+answer = gets.chomp.gsub(' ', '+')
 response = RestClient.get('http://google.com', {:params => {:q => answer}})
-puts response
+puts response.to_s
 puts response.code
 puts response.cookies
 puts response.body
+
+
+
+
+
